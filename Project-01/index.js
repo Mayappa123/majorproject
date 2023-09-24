3535517c6da324510e26d363e8badef089a9e2ff
Mayappa <mayappapujari561999@gmail.com>
2023-09-24 14:33:58 +0530

    we are created mathematical functions in index.js

--- Project-01/index.js
+++ Project-01/index.js
@@ -2,22 +2,24 @@ let sum = (a, b) => {
     return(a + b);
     console.log(sum);
   };
-  add(5, 10);
   
   let sub = (a, b) => {
       return(a - b);
     console.log(sub);
   };
-  sub(5, 10);
   
   let mul = (a, b) => {
       return(a * b);
     console.log(mul);
   };
-  mul(5, 10);
   
   let div = (a, b) => {
       return(a / b);
     console.log(div);
   };
+
+
+  sum(5, 10);
+  sub(5, 10);
+  mul(5, 10);
   div(5, 10);
