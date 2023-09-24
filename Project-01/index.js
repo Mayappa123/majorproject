9c59965ecd33576d019a1b0cc8c05e5667abad5e
Mayappa_Pujari <95636816+Mayappa123@users.noreply.github.com>
2023-09-24 14:24:33 +0530

    added mathematical code in index.js

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,2 +1,23 @@
-let a = 5;
-console.log(a);
+let sum = (a, b) => {
+  return(a + b);
+  console.log(sum);
+};
+add(5, 10);
+
+let sub = (a, b) => {
+    return(a - b);
+  console.log(sub);
+};
+sub(5, 10);
+
+let mul = (a, b) => {
+    return(a * b);
+  console.log(mul);
+};
+mul(5, 10);
+
+let div = (a, b) => {
+    return(a / b);
+  console.log(div);
+};
+div(5, 10);
