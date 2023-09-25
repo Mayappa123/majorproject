e1e089ec6162fd0fa08b37ad6d41c1b2a457d3c0
Mayappa <mayappapujari561999@gmail.com>
2023-09-25 17:31:18 +0530

    we updated index.js

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,28 +1,26 @@
-let sum = (a, b) => {
+const sum = (a, b) => {
     return(a + b);
-    console.log(sum);
   };
   
-let sub = (a, b) => {
+const sub = (a, b) => {
     return(a - b);
-    console.log(sub);
   };
   
-let mul = (a, b) => {
+const mul = (a, b) => {
     return(a * b);
-    console.log(mul);
   };
   
-let div = (a, b) => {
+const div = (a, b) => {
     return(a / b);
-    console.log(div);
   };
 
+  console.log(sum(5, 10));
+  console.log(sub(10, 5));
+  console.log(mul(5, 10));
+  console.log(div(10, 5));
 
-sum(5, 10);
-sub(5, 10);
-mul(5, 10);
-div(5, 10);
+
+
 
 
  
